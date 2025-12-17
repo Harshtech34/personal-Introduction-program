ff65cf3fa096129384eb17682b9c0bda4bb992f5
Harshtech34 <chavanharshal856@gmail.com>
2025-12-17 22:29:46 +0530

    Add files via upload
    Add screenshot of output

---
+++ personal_intro.py
@@ -0,0 +1,15 @@
+"""
+Personal Introduction Program
+Week 1 Internship Task - Python Basics
+"""
+
+print("Welcome to the Personal Introduction Program")
+
+# Taking user inputs
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+# Displaying output
+print("\nWelcome", name + "!")
+print("You are", age, "years old and love", hobby + ".")
